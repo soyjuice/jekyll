3939f0c57540122c91d7f716b08d582fa5b2cd90
soyjuice <1316692979@qq.com>
2016-11-08 14:57:16 +0800

    Add files via upload

---
+++ _site/js/merge.js
@@ -0,0 +1,12 @@
+﻿function merge_code(a)
+{
+	document.getElementById('code_pre'+a).style.display="inline";
+	document.getElementById('code_re'+a).style.display="none";
+	document.getElementById('code_set'+a).style.display="none";
+}
+function open_code(a)
+{
+	document.getElementById('code_pre'+a).style.display="none";
+	document.getElementById('code_re'+a).style.display="inline";
+	document.getElementById('code_set'+a).style.display="block";
+}
